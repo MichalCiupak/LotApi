bbfda1c8e644f5c5985b932d67c4569169c5e0b0
MichalCiupak <michalciupak.26@gmail.com>
2024-04-16 19:53:41 +0200

    Add getall and getbyid methodes for the Flights

---
+++ LotApi/Controllers/FlightController.cs
@@ -0,0 +1,61 @@
+using LotApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LotApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/stock")]
+    public class FlightController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public FlightController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var flights = _dataContext.Flight.ToList();
+            return Ok(flights);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var flight = _dataContext.Flight.Find(id);
+
+            if(flight == null)
+            {
+                return NotFound();
+            }
+            return Ok(flight);
+        }
+
+        /*private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }*/
+    }
+}
--- LotApi/Program.cs
+++ LotApi/Program.cs
@@ -4,12 +4,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-/*builder.Services.AddTransient<Seed>();*/
 builder.Services.AddDbContext<DataContext>( options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
