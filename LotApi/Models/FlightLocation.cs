3d1d12758d33cf968b91e6b8a04da74301351abd
MichalCiupak <michalciupak.26@gmail.com>
2024-04-16 10:29:37 +0200

    Add FlightLocation model

---
+++ LotApi/Models/FlightLocation.cs
@@ -0,0 +1,9 @@
+﻿namespace LotApi.Models
+{
+    public class FlightLocation
+    {
+        public string AirportName { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Country { get; set; } = null!;
+    }
+}
