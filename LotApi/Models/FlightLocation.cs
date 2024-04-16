6de5eedafb0d3fb6e9a3e3c865863304b71f6be8
MichalCiupak <michalciupak.26@gmail.com>
2024-04-16 11:46:05 +0200

    Add id to the FlightLocation model

--- LotApi/Models/FlightLocation.cs
+++ LotApi/Models/FlightLocation.cs
@@ -2,6 +2,7 @@
 {
     public class FlightLocation
     {
+        public int Id { get; set; }
         public string AirportName { get; set; } = null!;
         public string City { get; set; } = null!;
         public string Country { get; set; } = null!;
